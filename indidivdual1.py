f2c62a8fd897f7582ea4a032e6209378aa062fa5
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2022-05-02 13:23:09 +0300

    Update indidivdual1.py

--- indidivdual1.py
+++ indidivdual1.py
@@ -13,55 +13,50 @@ import math
 CONST_PRECISION = 1e-07
 
 
-def func_x(x=-0.7):
+def func_x(x):
     result = 1/(math.pow((1 - x), 2))
     return result
 
 
-def func_y(x=0.35):
+def func_y(x):
     result = 1/math.log10(math.sqrt((1 + x) / (1 - x)))
     return result
 
 
-def summ_1():
-    x = -0.7
-    pre = 0
-    s = 0
-    n = 0
-    curr = (n + 1) * math.pow(x, n)
-    s += curr
-    n += 1
-    while abs(curr - pre) > CONST_PRECISION:
-        pre = curr
-        curr = (n + 1) * math.pow(x, n)
+def summ_1(x):
+    n, s, m, curr = 0, 0, 0, 0
+    while True:
+        pre = (n + 1) * x**n
         n += 1
+        if abs(curr - pre) < CONST_PRECISION:
+            break
+        curr = (n + 1) * x**n
         s += curr
     return s
 
 
-def summ_2():
-    pre = 0
-    s = 0
-    n = 0
-    curr = math.pow(0.35, 2 * n + 1) / (2 * n + 1)
-    s += curr
-    n += 1
-    while abs(curr - pre) > CONST_PRECISION:
-        pre = curr
-        curr = math.pow(0.35, 2 * n + 1) / (2 * n + 1)
+def summ_2(x):
+    n, s, curr = 0, 0, 0
+    while True:
+        second = 2 * n + 1
+        first = x ** (2 * n + 1)
+        pre = first / second
         n += 1
+        if abs(curr - pre) < CONST_PRECISION:
+            break
+        curr = first / second
         s += curr
     return s
 
 
-def compare(x, y):
-    result = x - y
+def compare(first, second, x):
+    result = first(x) - second(x)
     print(f"Результат сравнения {result}")
 
 
 if __name__ == '__main__':
-    th1 = Thread(target=compare(summ_1(), func_x()))
-    th2 = Thread(target=compare(summ_2(), func_y()))
+    th1 = Thread(target=compare(summ_1, func_x, -0.7))
+    th2 = Thread(target=compare(summ_2, func_y, 0.35))
     th1.start()
     th2.start()
     th1.join()
